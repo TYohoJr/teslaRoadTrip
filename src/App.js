5179819087e025c458685a58acc3e00c381a48ba
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-28 18:14:52 -0500

    tetsing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.apiTest = this.apiTest.bind(this);
+  }
+
+  apiTest() {
+    axios.post("/apiTest", {test: 'test'}).then((result) => {
+      return console.log(result);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.apiTest}>API Test</button>
       </div>
     );
   }
